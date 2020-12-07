134a8bdca8f33a70730cb7a378c827e08a8bdbc1
Radu Constantin <raduadrian.constantin94@gmail.com>
2020-12-07 20:58:07 +0200

    Added db schema

---
+++ schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+id serial PRIMARY KEY,
+username varchar(30) UNIQUE NOT NULL,
+password varchar NOT NULL CHECK(LENGTH(password) > 8)
+);
+
+CREATE TABLE expenses (
+    id serial PRIMARY KEY,
+    name varchar(40) NOT NULL,
+    price numeric NOT NULL CHECK(price > 0),
+    wasted_check boolean DEFAULT true,
+    time_added date NOT NULL DEFAULT(CURRENT_TIMESTAMP)
+    user_id integer NOT NULL REFERENCES users(id)
+);
